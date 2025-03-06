193c4ba1402a6f128560f43cb5a965a739b2c9cf
gabrielsodre91 <gabrielsodre91@gmail.com>
2025-03-06 14:15:13 -0300

    fix: links fixed

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -7,13 +7,27 @@ export const Features = () => {
       <div className="container">
         <ScrollReveal>
           <div className="partners-logos">
-            <Image src="/images/dell-logo.png" alt="Dell" width={120} height={45} />
-            <Image src="/images/zendesk-logo.png" alt="Zendesk" width={120} height={45} />
-            <Image src="/images/rakuten-logo.png" alt="Rakuten" width={120} height={45} />
-            <Image src="/images/pacific-funds-logo.png" alt="Pacific Funds" width={120} height={45} />
-            <Image src="/images/ncr-logo.png" alt="NCR" width={120} height={45} />
-            <Image src="/images/lattice-logo.png" alt="Lattice" width={120} height={45} />
-            <Image src="/images/ted-logo.png" alt="TED" width={120} height={45} />
+            <a href="https://www.dell.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/dell-logo.png" alt="Dell" width={120} height={45} />
+            </a>
+            <a href="https://www.zendesk.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/zendesk-logo.png" alt="Zendesk" width={120} height={45} />
+            </a>
+            <a href="https://www.rakuten.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/rakuten-logo.png" alt="Rakuten" width={120} height={45} />
+            </a>
+            <a href="https://www.pacificlife.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/pacific-funds-logo.png" alt="Pacific Funds" width={120} height={45} />
+            </a>
+            <a href="https://www.ncr.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/ncr-logo.png" alt="NCR" width={120} height={45} />
+            </a>
+            <a href="https://www.lattice.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/lattice-logo.png" alt="Lattice" width={120} height={45} />
+            </a>
+            <a href="https://www.ted.com" target="_blank" rel="noopener noreferrer">
+              <Image src="/images/ted-logo.png" alt="TED" width={120} height={45} />
+            </a>
           </div>
         </ScrollReveal>
 
